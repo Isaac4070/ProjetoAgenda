0e68b4135eddb810d168883b8a3cd5bdb1208b83
isaac.nascimento <isaac.nascimento@dcx.ufpb.br>
2024-08-13 09:20:47 -0300

    Iniciando o projeto

---
+++ src/test/java/ContatoTest.java
@@ -0,0 +1,14 @@
+
+
+import br.ufpb.dcx.ayla.agenda.Contato;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class ContatoTest {
+
+    @Test
+    public void testaConstrutor(){
+        Contato c1 = new Contato("Ayla", 5, Setembro);
+
+    }
+}
