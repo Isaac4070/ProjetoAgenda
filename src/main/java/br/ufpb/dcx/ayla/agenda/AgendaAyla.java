83a3b1a0923d6e236aeba51eb5ccd960844f516c
isaac <isaacnascimento061@gmail.com>
2024-09-14 21:12:48 -0300

    Correção de erros.

--- src/main/java/br/ufpb/dcx/ayla/agenda/AgendaAyla.java
+++ src/main/java/br/ufpb/dcx/ayla/agenda/AgendaAyla.java
@@ -3,6 +3,7 @@ package br.ufpb.dcx.ayla.agenda;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class AgendaAyla implements Agenda {
 
@@ -14,29 +15,39 @@ public class AgendaAyla implements Agenda {
         this.gravador = new GravadorDeDados();
     }
 
-
     @Override
     public boolean cadastraContato(String nome, int dia, int mes) {
-        return false;
+        if (this.contatos.containsKey(nome)) {
+            return false; // Contato já existe
+        }
+        Contato novoContato = new Contato(nome, dia, mes);
+        this.contatos.put(nome, novoContato);
+        return true;
     }
 
     @Override
     public Collection<Contato> pesquisaAniversariantes(int dia, int mes) {
-        return null;
+        return this.contatos.values().stream()
+                .filter(contato -> contato.getDiaAniversario() == dia && contato.getMesAniversario() == mes)
+                .collect(Collectors.toList());
     }
 
     @Override
     public boolean removeContato(String nome) throws ContatoInexistenteException {
-        return false;
+        if (!this.contatos.containsKey(nome)) {
+            throw new ContatoInexistenteException(); // Contato não encontrado
+        }
+        this.contatos.remove(nome);
+        return true;
     }
 
     @Override
     public void salvarDados() throws IOException {
-
+        gravador.salvarContatos(this.contatos);
     }
 
     @Override
     public void recuperarDados() throws IOException {
-
+        this.contatos = gravador.recuperarContatos();
     }
 }
